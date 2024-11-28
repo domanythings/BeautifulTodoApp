2d43fc112c88f44502e61b67fdaa91cab7b091da
domanythings <tsolomon.ewiwilem@gmail.com>
2024-11-28 07:27:58 -0800

    update css

--- Todo/obj/Debug/net9.0/Todo.AssemblyInfo.cs
+++ Todo/obj/Debug/net9.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d315e18d2d3e63a7fcf8062b315b422419ad12b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
